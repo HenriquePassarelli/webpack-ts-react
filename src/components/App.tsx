81d9ce2a6c1f7b75a4e8cdad1205b45c94520459
Henrique Passarelli <henrique.passarellicc@gmail.com>
2022-04-12 22:39:27 -0300

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import './style.css'
+
+const App = () => {
+
+    return <h1>bye</h1>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 8080;
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.jsx', '.js', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader' }
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: ['url-loader']
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    'babel-loader',
+                    {
+                        loader: 'react-svg-loader',
+                        options: {
+                            jsx: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
+}
